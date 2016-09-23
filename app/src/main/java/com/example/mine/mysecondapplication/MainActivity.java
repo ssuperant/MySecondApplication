66943cafcd4689a4b9745827ac4f9f2bf5a138ee
Mine <812505952@qq.com>
2016-09-23 10:42:41 +0800

    2016.09.23修改，添加

--- app/src/main/java/com/example/mine/mysecondapplication/MainActivity.java
+++ app/src/main/java/com/example/mine/mysecondapplication/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.mine.mysecondapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends Activity {
 
@@ -12,5 +16,18 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+    }
+
+
+    private void mainActivity() {
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, Main2Activity.class));
+            }
+        };
+        timer.schedule(task, 1500);
     }
 }
